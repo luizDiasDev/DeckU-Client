9c78a63ee1c23ff7d4cd6bab1d62d062a78da690
Luiz Dias <luiz.dias602120@gmail.com>
2026-06-27 20:04:59 -0300

    chore: move sensitive values to .env

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gamepad import Gamepad
 from sender import Sender
+from dotenv import load_dotenv
+import os
 
 
 def main():
@@ -7,14 +9,20 @@ def main():
     Orquestra Gamepad e Sender conectando os dois via callback
     """
 
-    data_sender = Sender("192.168.100.54", 5005)
+    load_dotenv()
+
+    ip = os.getenv("PC_IP")
+    port = int(os.getenv("PORT"))
+    device = os.getenv("DEVICE_PATH")
+
+    data_sender = Sender(ip, port)
 
     # Recebe gamepad_output de Gamepad e repassa para Sender
     def callback(data):
     
         data_sender.send(data)
 
-    deck_gamepad = Gamepad("/dev/input/event10", callback)
+    deck_gamepad = Gamepad(device, callback)
 
     deck_gamepad.run()
 
